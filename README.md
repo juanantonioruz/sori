dddf76d5bdf06406af5b64c5c0a61a0167c4c4a4
juanantonioruz <juanantonioruz@gmail.com>
2012-11-19 12:52:31 +0100

    README changes

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
 # sori
 
 A website written in noir. 
+Here it is on-line
+
+http://el-sori.herokuapp.com/
 
 ## Usage
 
@@ -11,7 +14,7 @@ lein run
 
 ## License
 
-Copyright (C) 2011 FIXME
+Copyright (C) 2011 juanantonioruz@gmail.com
 
 Distributed under the Eclipse Public License, the same as Clojure.
 
--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
 (defproject sori "0.1.0-SNAPSHOT"
-            :description "FIXME: write this!"
+            :description "Clojure WebNoir Project"
             :dependencies [[org.clojure/clojure "1.3.0"]
-                           [noir "1.2.1"]]
+                           [noir "1.2.1"]
+                           [congomongo "0.3.2"]]
             :main sori.server)
 
